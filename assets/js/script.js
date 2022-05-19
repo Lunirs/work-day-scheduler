f2c7d8e61cf702b284c1adf70b8e688773c2d5e6
Lunirs <dhong0925@gmail.com>
2022-05-19 00:35:31 -0400

    Added for loop to change timeblock color based on past, present, future of current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+
+var currentTime = moment().hours();
+
+for (i = 0; i < 25; i++) {
+  if (currentTime === i) {
+    $(".container").children().eq(i).children().eq(1).addClass("present");
+  } else if (currentTime > i) {
+    $(".container").children().eq(i).children().eq(1).addClass("past");
+  } else {
+    $(".container").children().eq(i).children().eq(1).addClass("future");
+  }
+}
