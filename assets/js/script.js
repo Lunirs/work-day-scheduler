8c249af485547e05057c4fd071dae5bc464315a2
Lunirs <dhong0925@gmail.com>
2022-05-19 00:43:16 -0400

    Added eventlistener to save text value to local storage and display the stored text even on page refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,35 @@ for (i = 0; i < 25; i++) {
     $(".container").children().eq(i).children().eq(1).addClass("future");
   }
 }
+
+$(".saveBtn").on("click", function () {
+  var text = $(this).siblings(".description").val();
+  var hr = $(this).parent().attr("id");
+
+  localStorage.setItem(hr, text);
+});
+
+$("#hr1 .description").val(localStorage.getItem("hr1"));
+$("#hr2 .description").val(localStorage.getItem("hr2"));
+$("#hr3 .description").val(localStorage.getItem("hr3"));
+$("#hr4 .description").val(localStorage.getItem("hr4"));
+$("#hr5 .description").val(localStorage.getItem("hr5"));
+$("#hr6 .description").val(localStorage.getItem("hr6"));
+$("#hr7 .description").val(localStorage.getItem("hr7"));
+$("#hr8 .description").val(localStorage.getItem("hr8"));
+$("#hr9 .description").val(localStorage.getItem("hr9"));
+$("#hr10 .description").val(localStorage.getItem("hr10"));
+$("#hr11 .description").val(localStorage.getItem("hr11"));
+$("#hr12 .description").val(localStorage.getItem("hr12"));
+$("#hr13 .description").val(localStorage.getItem("hr13"));
+$("#hr14 .description").val(localStorage.getItem("hr14"));
+$("#hr15 .description").val(localStorage.getItem("hr15"));
+$("#hr16 .description").val(localStorage.getItem("hr16"));
+$("#hr17 .description").val(localStorage.getItem("hr17"));
+$("#hr18 .description").val(localStorage.getItem("hr18"));
+$("#hr19 .description").val(localStorage.getItem("hr19"));
+$("#hr20 .description").val(localStorage.getItem("hr20"));
+$("#hr21 .description").val(localStorage.getItem("hr21"));
+$("#hr22 .description").val(localStorage.getItem("hr22"));
+$("#hr23 .description").val(localStorage.getItem("hr23"));
+$("#hr24 .description").val(localStorage.getItem("hr24"));
